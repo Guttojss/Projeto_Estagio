e5b2059e9339cf5ff9b09a94458b4ebec4edebb2
Guttojss <guttojsstfg@gmail.com>
2024-07-05 08:16:35 +0100

    correção de bugs

--- script.js
+++ script.js
@@ -1,42 +1,46 @@
-const FORM = document.getElementById("FORM").addEventListener('submit',function(event)
-{
-    event.preventDefault();
-    const URL = document.getElementById("URL")
-    safe(URL);
-    fast(URL);
-    codigo(URL);
-
-    console.log("URL inserido : ",URL.value);
-});
-
-//Verifica a velocidade da URL
-function fast(URL){
-    const start = performance.now();
-    fetch(URL.value)
-    .then(response => {
-            const end = performance.now();  // Tempo de término
-            const duration = end - start;  // Cálculo da duração
-            console.log("Tempo de carregamento: ", duration, "ms");
-        }
-    )
-}
-
-//Verifica se o URL possui "https://"
-function safe(URL){
-    fetch(URL.value)
-    .then(response => 
+    const FORM = document.getElementById("FORM").addEventListener('submit',function(event)
         {
-            console.log("Protocolo : ",document.location.protocol)
-        }
-    )
-}
+            event.preventDefault();
+            const URL = document.getElementById("URL")
 
-//Verifica o código 
-function codigo(URL)
-{
-    fetch(URL.value).then(Response =>
-        {
-          console.log("Código = ", Response.status);
+            safe(URL);
+            fast(URL);
+            codigo(URL);
+
+            console.log("URL inserido : ",URL.value);
         }
     )
-}
+
+    //Verifica a velocidade da URL
+    function fast(URL){
+        const start = performance.now();
+        fetch(URL.value)
+        .then(response => 
+            {
+                const end = performance.now();
+                const duration = end - start;  
+                console.log("Tempo de carregamento: ", duration, "ms");
+            }
+        )
+    }
+
+    //Verifica se o URL possui "https://"
+    //pode não funcionar por causa do "Document.location.protocol".
+    function safe(URL){
+        fetch(URL.value)
+        .then(response => 
+            {
+                console.log("Protocolo : ",document.location.protocol)
+            }
+        )
+    }
+
+    //Verifica o código 
+    function codigo(URL)
+    {
+        fetch(URL.value).then(Response =>
+            {
+            console.log("Código = ", Response.status);
+            }
+        )
+    }
