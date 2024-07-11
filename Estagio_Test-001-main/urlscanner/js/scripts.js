efe4cbd28659cb10735609ac8af4a22bf6cc3347
Guttojss <guttojsstfg@gmail.com>
2024-07-11 09:05:18 +0100

    juntei o back com o front (Alexffb)

---
+++ Estagio_Test-001-main/urlscanner/js/scripts.js
@@ -0,0 +1,61 @@
+  
+// FEITO PARA FORMS
+///    const FORM = document.getElementById("FORM").addEventListener('submit',function(event)
+//    {
+//        event.preventDefault(); //retira o reload automatico
+///        const URL = document.getElementById("URL")
+///
+ //       safe(URL);
+ //       fast(URL);
+   //     codigo(URL); 
+//
+   //     console.log("URL inserido : ",URL.value);
+  //  }
+//)
+const button = document.getElementById("submitButton").addEventListener("click",function(event)
+{
+    event.preventDefault(); //retira o reload automatico
+    const URL = document.getElementById("URL")
+    safe(URL);
+    fast(URL);
+    codigo(URL); 
+
+    console.log("URL inserido : ",URL.value);
+}
+)
+
+//FEITO PARA O BUTÃO
+
+//Verifica a velocidade da URL
+function fast(URL){
+    const start = performance.now();
+    fetch(URL.value)
+    .then(response => 
+        {
+            const end = performance.now();
+            const duration = end - start;  
+            console.log("Tempo de carregamento: ", duration, "ms");
+        }
+    )
+}
+
+//Verifica se o URL possui "https://"
+//pode não funcionar por causa do "Document.location.protocol".
+function safe(URL){
+    fetch(URL.value)
+    .then(response => 
+        {
+            console.log("Protocolo : ", document.location.protocol)
+        }
+    )
+}
+
+//Verifica o código 
+function codigo(URL)
+{
+    fetch(URL.value).then(Response =>
+        {
+        console.log("Código = ", Response.status);
+        }
+    )
+}
